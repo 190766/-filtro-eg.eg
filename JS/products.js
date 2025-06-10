2cfeba761d23c4ee758cb835e069d5e9851ea3c9
190766 <aliprogramer96@gmail.com>
2025-06-10 03:48:14 +0300

    Add files via upload

---
+++ JS/products.js
@@ -0,0 +1,57 @@
+//  $(".size").on('click', function(){
+//     $(this).toggleClass('focus').siblings().removeClass('focus');
+//  })
+//    const addToCartButtons = document.querySelectorAll(".btn_add_cart")
+
+//     addToCartButtons.forEach(button =>{
+//         button.addEventListener("click", (event) => {
+//             const productId = event.target.getAttribute('data-id')
+//             const selcetedProduct = data.find(product => product.id == productId)
+            
+
+//             addToCart(selcetedProduct)
+
+//             const allMatchingButtons = document.querySelectorAll(`.btn_add_cart[data-id="${productId}"]`)
+
+//             allMatchingButtons.forEach(btn =>{
+//                 btn.classList.add("active")
+//                 btn.innerHTML = `      <i class="fa-solid fa-cart-shopping"></i> Item in cart`
+//             })
+//         })
+//     })
+    
+ $(".size").on('click', function(){
+    $(this).toggleClass('focus').siblings().removeClass('focus');
+ })
+   const addToCartButtons = document.querySelectorAll(".btn_add_cart")
+
+    addToCartButtons.forEach(button =>{
+        button.addEventListener("click", (event) => {
+            const productId = event.target.getAttribute('data-id')
+            const selcetedProduct = data.find(product => product.id == productId)
+            
+
+            addToCart(selcetedProduct)
+
+            const allMatchingButtons = document.querySelectorAll(`.btn_add_cart[data-id="${productId}"]`)
+
+            allMatchingButtons.forEach(btn =>{
+                btn.classList.add("active")
+                btn.innerHTML = `      <i class="fa-solid fa-cart-shopping"></i> Item in cart`
+            })
+        })
+    })
+function addToCart(product) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+    const existingProductIndex = cart.findIndex(item => item.id === product.id)
+
+    if (existingProductIndex > -1) {
+        cart[existingProductIndex].quantity += 1
+    } else {
+        product.quantity = 1
+        cart.push(product)
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
